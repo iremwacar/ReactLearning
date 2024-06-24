deb81852cffba5a1a8143d04312b8c41d5bf8765
irem acar <iremmacar35@gmail.com>
2024-06-24 15:19:52 +0300

    react lifecycle commit

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -30,7 +30,7 @@ export default class CategoryList extends Component {
             </ListGroupItem>
           ))}
         </ListGroup>
-        <h4>{this.props.currentCategory}</h4>
+        {/* <h4>{this.props.currentCategory}</h4> */}
       </div>
     );
   }
