baa894a616e1c6c77fca7860abae60f249901dda
irem acar <iremmacar35@gmail.com>
2024-06-24 14:33:38 +0300

    product commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
 
 export default class App extends Component {
-  state={currentCategory:""}
+  state={currentCategory:"",products:[]}
+
+  componentDidMount(){
+    this.getProducts();
+  }
 
   changeCategory = (category)=>{
     this.setState({currentCategory:category.categoryName})
   };
+
+  getProducts = () =>{
+    fetch("http://localhost:3000/products")
+    .then(response=>response.json())
+    .then(data=>this.setState({products:data}));;
+  }
 
   render() {
     let productInfo = { title: "Product List" };
@@ -30,6 +40,7 @@ export default class App extends Component {
             </Col>
             <Col xs="9">
               <ProductList
+              products={this.state.products}
               currentCategory={this.state.currentCategory} 
               info={productInfo} />
             </Col>
