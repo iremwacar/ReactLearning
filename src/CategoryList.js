1e95610e70b3456009393027f5ed279f7c2e69a8
irem acar <iremmacar35@gmail.com>
2024-06-24 13:39:25 +0300

    event commit

---
+++ src/CategoryList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+export default class CategoryList extends Component {
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiements" },
+      { categoryId: 3, categoryName: "Enother" },
+    ]
+  };
+
+  
+
+  render() {
+    return (
+      <div>
+        <h4>{this.props.info.title}</h4>
+        <ListGroup>
+          {this.state.categories.map((category) => (
+            <ListGroupItem 
+            onClick={()=>this.props.changeCategory(category)}
+            key={category.categoryId}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <h4>{this.props.currentCategory}</h4>
+      </div>
+    );
+  }
+}
