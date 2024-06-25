80a2336c93f28e2e1bc24bd8380e75eb07c227df
irem acar <iremmacar35@gmail.com>
2024-06-25 12:16:25 +0300

    add product to cart commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,37 @@ import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
 
 export default class App extends Component {
-  state={currentCategory:"",products:[]}
+  state = { currentCategory: "", products: [], cart:[] };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getProducts();
   }
 
-  changeCategory = (category)=>{
-    this.setState({currentCategory:category.categoryName});
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
     this.getProducts(category.id);
   };
 
-  getProducts = (categoryId) =>{
-    let url="http://localhost:3000/products";
-    if(categoryId){
-      url+="?categoryId="+categoryId;
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
     }
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState({products:data}));
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  addToCart=(product)=>{
+    let newCart =  this.state.cart;
+    var addedItem = newCart.find(c=>c.product.id===product.id);
+    if(addedItem){
+      addedItem.quantity+=1;
+    }
+    else{
+      newCart.push({product:product,quantity:1});
+    }
+    this.setState({cart:newCart});
   }
 
   render() {
@@ -32,22 +44,23 @@ export default class App extends Component {
     return (
       <div>
         <Container>
-          <Row>
-            <Navi />
-          </Row>
+          <Navi cart={this.state.cart}/>
 
           <Row>
             <Col xs="3">
-              <CategoryList 
-              currentCategory={this.state.currentCategory}
-              changeCategory={this.changeCategory}
-              info={categoryInfo} />
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
               <ProductList
-              products={this.state.products}
-              currentCategory={this.state.currentCategory} 
-              info={productInfo} />
+                products={this.state.products}
+                addToCart={this.addToCart}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
             </Col>
           </Row>
         </Container>
--- src/Navi.js
+++ src/Navi.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react'
-
-export default class Navi extends Component {
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+ 
+export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
   render() {
     return (
       <div>
-        <h1>Navi</h1>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Northwind App</NavbarBrand>
+          <NavbarText>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components">components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">GitHub</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    options -{this.props.cart.length}
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>option1</DropdownItem>
+                    <DropdownItem>option2</DropdownItem>
+                    <DropdownItem>Reset</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </NavbarText>
+        </Navbar>
       </div>
-    )
+    );
   }
 }
